a60f9620a9a8a1a18af213a1542203f5df34a5bd
Lenkovski <10lenkovski10@gmail.com>
2025-06-14 16:15:45 +0200

    Update hw_01.py

--- hw_01.py
+++ hw_01.py
@@ -1,18 +1,18 @@
 def caching_fibonacci():
-    cache = {}  # Словник для кешування обчислених значень
+    cache = {} 
 
     def fibonacci(n):
-        # Базові випадки
+      
         if n <= 0:
             return 0
         if n == 1:
             return 1
         
-        # Якщо значення вже є в кеші - повертаємо його
+       
         if n in cache:
             return cache[n]
         
-        # Інакше обчислюємо рекурсивно, зберігаємо у кеш і повертаємо
+        
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
         return cache[n]
 
@@ -20,6 +20,7 @@ def caching_fibonacci():
 
 fib = caching_fibonacci()
 
-print(fib(10))  # Виведе 55
-print(fib(15))  # Виведе 610
-print(fib(20))  # Виведе 6765
+print(fib(10)) 
+print(fib(15)) 
+print(fib(20)) 
+print(fib(30))   
